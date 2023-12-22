f6d1fa35936e9261b88c2ff54fb8186b6ddeeae2
Jennifer Sivapalan <944375+jennysivapalan@users.noreply.github.com>
2023-12-22 14:03:24 +0000

    refactor: removed export as not needed

--- client/src/components/misdemeanours/item.tsx
+++ client/src/components/misdemeanours/item.tsx
@@ -3,7 +3,7 @@ import { Misdemeanour } from "../../types/misdemeanours.types";
 interface MisdemeanourProps {
   misdemeanour: Misdemeanour;
 }
-export const Item: React.FC<MisdemeanourProps> = ({ misdemeanour }) => {
+const Item: React.FC<MisdemeanourProps> = ({ misdemeanour }) => {
   return (
     <div>
       {misdemeanour.citizenId}, {misdemeanour.date}, {misdemeanour.misdemeanour}
