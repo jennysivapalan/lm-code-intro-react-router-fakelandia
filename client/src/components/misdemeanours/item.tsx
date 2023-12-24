c098c915fe2abbb9eb0d39c0e04d1133a9be8603
Jennifer Sivapalan <944375+jennysivapalan@users.noreply.github.com>
2023-12-24 14:25:09 +0000

    feat: a random photo is used as a punishment idea from the picsum service for a misdeameanour

--- client/src/components/misdemeanours/item.tsx
+++ client/src/components/misdemeanours/item.tsx
@@ -2,12 +2,20 @@ import { Misdemeanour } from "../../types/misdemeanours.types";
 
 interface MisdemeanourProps {
   misdemeanour: Misdemeanour;
+  imageId: string;
 }
-const Item: React.FC<MisdemeanourProps> = ({ misdemeanour }) => {
+const Item: React.FC<MisdemeanourProps> = ({ misdemeanour, imageId }) => {
+  const imageUrl = `https://picsum.photos/id/${imageId}/200/200`;
   return (
-    <div className="misdeameanour-item">
-      {misdemeanour.citizenId}, {misdemeanour.date}, {misdemeanour.misdemeanour}
-    </div>
+    <>
+      <div className="misdeameanour-item">
+        {misdemeanour.citizenId}, {misdemeanour.date},{" "}
+        {misdemeanour.misdemeanour}
+      </div>
+      <div>
+        <img src={imageUrl} />
+      </div>
+    </>
   );
 };
 
--- client/src/components/misdemeanours/list.tsx
+++ client/src/components/misdemeanours/list.tsx
@@ -1,6 +1,7 @@
 import Item from "./item";
 import { Misdemeanour } from "../../types/misdemeanours.types";
 import { v4 as uuid } from "uuid";
+import { useEffect, useState } from "react";
 
 interface MisdemeanourListProps {
   misdemeanours: Misdemeanour[];
@@ -9,11 +10,28 @@ interface MisdemeanourListProps {
 const MisdemeanoursList: React.FC<MisdemeanourListProps> = ({
   misdemeanours,
 }) => {
+  const endpoint = `https://picsum.photos/v2/list?limit=${misdemeanours.length}`;
+  const [imageIds, setImageIds] = useState<string[]>([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(endpoint);
+        const json = await response.json();
+        const ids = json.map((j) => j.id);
+
+        setImageIds(ids);
+      } catch (error) {
+        console.error("Error", error);
+      }
+    };
+
+    fetchData();
+  }, []);
   return (
     <>
       <ul className="misdeameanours-list">
-        {misdemeanours.map((m) => (
-          <Item misdemeanour={m} key={uuid()} />
+        {misdemeanours.map((m, i) => (
+          <Item misdemeanour={m} key={uuid()} imageId={imageIds[i]} />
         ))}
       </ul>
     </>
