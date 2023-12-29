b714c2f00e785c8007ea05c1f7dc70bf99531d8d
Jennifer Sivapalan <944375+jennysivapalan@users.noreply.github.com>
2023-12-29 12:18:07 +0000

    feat: add emoji to the misdemeanour

--- client/src/components/misdemeanours/item.tsx
+++ client/src/components/misdemeanours/item.tsx
@@ -1,4 +1,4 @@
-import { Misdemeanour } from "../../types/misdemeanours.types";
+import { Misdemeanour, EMOJIS } from "../../types/misdemeanours.types";
 
 interface MisdemeanourProps {
   misdemeanour: Misdemeanour;
@@ -6,12 +6,17 @@ interface MisdemeanourProps {
 }
 const Item: React.FC<MisdemeanourProps> = ({ misdemeanour, imageId }) => {
   const imageUrl = `https://picsum.photos/id/${imageId}/200/200`;
+  const emoji = EMOJIS.find(
+    (e) => e.misdemeanour === misdemeanour.misdemeanour
+  )?.emoji;
   return (
     <>
       <div className="table-row">
         <div className="row-item row-item__first">{misdemeanour.citizenId}</div>
         <div className="row-item">{misdemeanour.date}</div>
-        <div className="row-item">{misdemeanour.misdemeanour}</div>
+        <div className="row-item">
+          {misdemeanour.misdemeanour} {emoji}
+        </div>
         <div className="row-item">
           <img src={imageUrl} />
         </div>
--- client/src/types/misdemeanours.types.ts
+++ client/src/types/misdemeanours.types.ts
@@ -1,16 +1,27 @@
 export const MISDEMEANOURS = [
-	'rudeness',
-	'vegetables',
-	'lift',
-	'united',
+  "rudeness",
+  "vegetables",
+  "lift",
+  "united",
 ] as const;
 export type MisdemeanourKind = (typeof MISDEMEANOURS)[number];
 
-export const JUST_TALK = 'just-talk';
+export const JUST_TALK = "just-talk";
 export type JustTalk = typeof JUST_TALK;
 
 export type Misdemeanour = {
-	citizenId: number;
-	misdemeanour: MisdemeanourKind;
-	date: string; // we'll stringify this for easy sending via HTTP rather than storing the full Date object
+  citizenId: number;
+  misdemeanour: MisdemeanourKind;
+  date: string; // we'll stringify this for easy sending via HTTP rather than storing the full Date object
 };
+
+type MisdemeanourEmojis = {
+  misdemeanour: MisdemeanourKind;
+  emoji: string;
+};
+export const EMOJIS: MisdemeanourEmojis[] = [
+  { misdemeanour: "rudeness", emoji: "🤪" },
+  { misdemeanour: "vegetables", emoji: "🥗" },
+  { misdemeanour: "lift", emoji: "🗣" },
+  { misdemeanour: "united", emoji: "😈" },
+];
