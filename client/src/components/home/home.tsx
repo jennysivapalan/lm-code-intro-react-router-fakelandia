4cc7f16cb0c2c550faca40bfba05c1c32209560c
Jennifer Sivapalan <944375+jennysivapalan@users.noreply.github.com>
2024-01-02 17:12:05 +0000

    feat: Added home page info

--- client/src/components/home/home.tsx
+++ client/src/components/home/home.tsx
@@ -1,3 +1,13 @@
-const Home: React.FC = () => <>Home!</>;
+const Home: React.FC = () => (
+  <>
+    <section className="intro">
+      <div>Welcome to the home of the Justice Department of Fakelandia</div>
+      <div>
+        Here you can browse a list of recent misdemeanours committed by our
+        citizens, or you can confess to your own misdemeanour.
+      </div>
+    </section>
+  </>
+);
 
 export default Home;
