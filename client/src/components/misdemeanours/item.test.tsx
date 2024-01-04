ebcaf939454bd0c96e379828da5a002ae24cddcf
Jennifer Sivapalan <944375+jennysivapalan@users.noreply.github.com>
2024-01-04 18:13:49 +0000

    test: render item misdemeanour component

---
+++ client/src/components/misdemeanours/item.test.tsx
@@ -0,0 +1,19 @@
+import { Misdemeanour } from "../../types/misdemeanours.types";
+import { render, screen } from "@testing-library/react";
+import Item from "./item";
+const dummyMisdemeanour: Misdemeanour = {
+  citizenId: 123,
+  misdemeanour: "vegetables",
+  date: "22/12/2023",
+};
+
+test.only("renders misdeameanours item component", () => {
+  const { container } = render(
+    <Item misdemeanour={dummyMisdemeanour} key={"123"} imageId={"abcd"} />
+  );
+  expect(container.getElementsByClassName("table-row").length).toBe(1);
+  expect(container.getElementsByClassName("row-item").length).toBe(4);
+  expect(screen.findAllByText(123)).toBeInTheDocument;
+  expect(screen.findAllByText("vegetables")).toBeInTheDocument;
+  expect(screen.findAllByText("22/12/2023")).toBeInTheDocument;
+});
