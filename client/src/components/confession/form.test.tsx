7304bbda1d2820a21afdc849306c35cfda9c534c
Jennifer Sivapalan <944375+jennysivapalan@users.noreply.github.com>
2024-01-04 19:02:16 +0000

    test: confession form renders success or error message after POST request

--- client/src/components/confession/form.test.tsx
+++ client/src/components/confession/form.test.tsx
@@ -1,5 +1,21 @@
 import Form from "./form";
 import { render, screen, fireEvent } from "@testing-library/react";
+import { http, HttpResponse } from "msw";
+import { setupServer } from "msw/node";
+
+const server = setupServer();
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
+
+const validPostResponse = http.post("http://localhost:8080/api/confess", () =>
+  HttpResponse.json({ success: true, message: "Success!" })
+);
+
+const errorPostResponse = http.post("http://localhost:8080/api/confess", () =>
+  HttpResponse.json({ success: false, message: "Fail!" })
+);
 
 describe("form details", () => {
   test("renders button and it is disabled", () => {
@@ -10,7 +26,8 @@ describe("form details", () => {
     expect(button).toHaveAttribute("disabled");
   });
 
-  test("button to be enabled when fields are filled", () => {
+  test("button to be enabled when fields are filled and confirmation given when submitted", async () => {
+    server.use(validPostResponse);
     render(<Form />);
     const subject = screen.getByTestId("subject");
     fireEvent.change(subject, {
@@ -29,6 +46,36 @@ describe("form details", () => {
 
     expect(button).toBeInTheDocument();
     expect(button).not.toHaveAttribute("disabled");
+
+    fireEvent.click(button);
+    expect(await screen.findAllByText("Success!")).toBeInTheDocument;
+  });
+
+  test("error message is shown if post request fails", async () => {
+    server.use(errorPostResponse);
+    render(<Form />);
+    const subject = screen.getByTestId("subject");
+    fireEvent.change(subject, {
+      target: { value: "i have something to report" },
+    });
+
+    const reason = screen.getByTestId("reason");
+    fireEvent.change(reason, { target: { value: "united" } });
+
+    const details = screen.getByTestId("details");
+    fireEvent.change(details, {
+      target: { value: "Citizen 452 loves united and I am reporting them" },
+    });
+
+    const button = screen.getByRole("button");
+
+    fireEvent.click(button);
+    expect(await screen.findAllByText("Fail!")).toBeInTheDocument;
+    expect(
+      await screen.findAllByText(
+        "An error occured when confessing, please try again."
+      )
+    ).toBeInTheDocument;
   });
 
   test("button is disabled when subject is missing", () => {
