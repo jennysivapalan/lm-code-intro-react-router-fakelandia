06867ebcd857c15b77524d1bd17916ad66ee869f
Jennifer Sivapalan <944375+jennysivapalan@users.noreply.github.com>
2024-01-07 16:02:01 +0000

    wip

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,40 @@
 import "./App.css";
 import { BrowserRouter } from "react-router-dom";
 import Router from "./router/router";
+import { useEffect, useState, createContext } from "react";
+import { Misdemeanour } from "./types/misdemeanours.types";
+import { newMisdemeanours } from "./service/new-confessions";
+import { fetchMisdemeanours } from "./service/fetch-misdemeanours";
+
+const MisdemeanoursContext = createContext<{
+  misdemeanour: Misdemeanour[];
+}>(<Misdemeanour[]>[]); //initialize context with default value
 
 function App() {
+  const [misdemeanours, setMisdemeanours] = useState<Misdemeanour[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const json = await fetchMisdemeanours();
+        const allMisdemeanours = json.concat(newMisdemeanours);
+
+        setMisdemeanours(allMisdemeanours);
+      } catch (error) {
+        console.error("Error", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <>
-      <BrowserRouter>
-        <Router />
-      </BrowserRouter>
+      <MisdemeanoursContext.Provider value={{ misdemeanours }}>
+        <BrowserRouter>
+          <Router />
+        </BrowserRouter>
+      </MisdemeanoursContext.Provider>
     </>
   );
 }
--- client/src/components/misdemeanours/misdemeanours.tsx
+++ client/src/components/misdemeanours/misdemeanours.tsx
@@ -1,35 +1,19 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useContext } from "react";
 import { Misdemeanour } from "../../types/misdemeanours.types";
 import MisdemeanoursList from "./list";
 import MisdemeanoursSelect from "./select";
 import { newMisdemeanours } from "../../service/new-confessions";
 import { fetchMisdemeanours } from "../../service/fetch-misdemeanours";
 
+import MisdemeanoursContext from "../../App";
+
 const Misdemeanours: React.FC = () => {
-  const [misdemeanours, setMisdemeanours] = useState<Misdemeanour[]>([]);
+  const x: Misdemeanour[] = useContext(MisdemeanoursContext);
+
   const [filteredMisdemeanours, setFilteredMisdemeanours] =
     useState<Misdemeanour[]>(misdemeanours);
   const [selectedOption, setSeletedOption] = useState<string>("all");
   const [errorMsg, setErrorMsg] = useState<string>("");
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const json = await fetchMisdemeanours();
-        const allMisdemeanours = json.concat(newMisdemeanours);
-
-        setMisdemeanours(allMisdemeanours);
-        setFilteredMisdemeanours(allMisdemeanours);
-      } catch (error) {
-        console.error("Error", error);
-        setErrorMsg(
-          "Sorry an error occurred fetching misdemeanours. Try again later."
-        );
-      }
-    };
-
-    fetchData();
-  }, []);
 
   function updateFilteredMisdemeanour(selectedMisdemeanour: string) {
     if (selectedMisdemeanour === "all") setFilteredMisdemeanours(misdemeanours);
